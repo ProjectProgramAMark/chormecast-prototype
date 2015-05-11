64c2e60196fe3bda1fdd2ad1b4d8907750b24f6e
Will Dent <will@dent.ly>
2015-05-11 12:18:10 -0400

    Updated app to work with Android and fixed issue #1

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,9 +1,11 @@
-function doClick(e) {
+/*function doClick(e) {
     var secondViewController = Alloy.createController('videoPlayer');
     
     var secondWindow = secondViewController.getView();
     
     secondWindow.open();
 }
+*/
+//http://download.wavetlan.com/SVV/Media/HTTP/http-mp4.htm
 
 $.index.open();
